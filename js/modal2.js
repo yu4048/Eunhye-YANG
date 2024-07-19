f61eef1992dc81ee3f30187eba649d3af3f07c9a
yu4048 <nyet311@gmail.com>
2024-07-19 11:15:34 +0900

    모달진행

--- js/modal2.js
+++ js/modal2.js
@@ -36,3 +36,45 @@ $(document).ready(function(){
   });
   
 });
+
+
+$(document).ready(function(){
+
+  //각 목록을 클릭했을때.....
+  $(".item_list2>li").click(function(){
+    g_pop=$(this).index();
+
+    $(".gp2 span:nth-child(1)").text(g_pop+1);  //오른쪽 상단 페이지번호
+    $("html").css({"overflow-y":"hidden"}); //기존 html 스크롤 숨기기
+    $(".mc2>li").eq(g_pop).fadeIn();  //각 목록의 내용
+    $(".pb2").stop().fadeIn(); //검정배경
+  });
+
+  //이전다음버튼....
+  $(".pr2").click(function(){
+    if(g_pop>0){
+      $(".mc2>li").eq(g_pop).hide();
+      g_pop--;
+      $(".gp2 span:nth-child(1)").text(g_pop+1);
+			$(".mc2>li").eq(g_pop).stop().show();
+    };
+  });
+  $(".nx2").click(function(){
+    if(g_pop<4){
+      $(".mc2>li").eq(g_pop).hide();
+      g_pop++;
+      $(".gp2 span:nth-child(1)").text(g_pop+1);
+			$(".mc2>li").eq(g_pop).stop().show();
+    };
+  });
+
+  //닫기버튼.....
+  $(".cb2").click(function(){
+    $("html").css({"overflow-y":"scroll"}); 
+    $(".pb2").stop().fadeOut();
+		$(".mc2>li").stop().fadeOut();
+  });
+  
+});
+
+
